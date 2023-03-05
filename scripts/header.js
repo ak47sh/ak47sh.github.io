98867914db828e6d21fb7ab28ad10aca7808cabf
ak47sh <akash.ni@pm.me>
2023-03-05 17:34:02 -0500

    a

--- scripts/header.js
+++ scripts/header.js
@@ -2,22 +2,39 @@ document.addEventListener("DOMContentLoaded", function() {
     // code to run when the DOM is ready
     const headerthing = document.createElement("div");
     headerthing.innerHTML = `
-    <nav class="container border-bottom">
-    <div class="d-flex justify-content-between align-items-center mt-4 mb-3">
-      <a href="index.html" class="text-decoration-none"><div style="color: white;" class="navbrand p-2 me-5">kasu</div></a>
-      <div class="d-flex justify-content-center">
-        <a href="about.html" class="text-decoration-none"><div style="color: white;" class="p-2 mx-4">about us</div></a>
-        <a href="stores.html" class="text-decoration-none"><div style="color: white;" class="p-2 mx-4">stores</div></a>
-        <a href="FAQ.html" class="text-decoration-none"><div style="color: white;" class="p-2 mx-4">FAQ</div></a>
-        <a href="contact.html" class="text-decoration-none"><div style="color: white;" class="p-2 mx-4">contact us</div></a>
-      </div>  
-      <div class="d-flex align-items-center">
-        <div class="me-4">signup</div>
-        <div class="ms-4">login</div>
-      </div>  
+    <nav class="container navbar navbar-expand-xl navbar-dark border-bottom">
+    <a href="index.html" class="navbar-brand me-5 text-decoration-none">
+      <div style="color: white;" class="p-2">kasu</div>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
+      <div class="navbar-nav centerabousoulte mx-auto" style="width: fit-content;">
+        <a href="about.html" class="nav-link text-decoration-none">
+          <div style="color: white;" class="p-2 ms-5 mx-4">about us</div>
+        </a>
+        <a href="stores.html" class="nav-link text-decoration-none">
+          <div style="color: white;" class="p-2 mx-4">stores</div>
+        </a>
+        <a href="FAQ.html" class="nav-link text-decoration-none">
+          <div style="color: white;" class="p-2 mx-4">FAQ</div>
+        </a>
+        <a href="contact.html" class="nav-link text-decoration-none">
+          <div style="color: white;" class="p-2 mx-4">contact us</div>
+        </a>
+      </div>
+      <div class="navbar-nav ms-auto ml-auto">
+        <a href="signup.html" class="nav-link text-decoration-none">
+          <div style="color: white;" class="p-2 me-4">signup</div>
+        </a>
+        <a href="login.html" class="nav-link text-decoration-none">
+          <div style="color: white;" class="p-2 ms-4">login</div>
+        </a>
+      </div>
     </div>
   </nav>
-    `.trim();
+          `.trim();
     headerthing.classList.add("headerthing"); // add the "my-class" class
     document.body.insertBefore(headerthing, document.body.firstChild);
 
